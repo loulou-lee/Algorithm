b06165376ddd45e1152e566bd517f83b74fd3e22
eunsooklee <98075420+loulou-lee@users.noreply.github.com>
2023-02-03 15:01:36 +0900

    Update select.sql

--- SQL/select.sql
+++ SQL/select.sql
@@ -3,3 +3,11 @@
 SELECT ROUND(AVG(DAILY_FEE)) AS AVERAGE_FEE
 FROM CAR_RENTAL_COMPANY_CAR
 WHERE CAR_TYPE = 'SUV'
+
+# DOCTOR 테이블에서 진료과가 흉부외과(CS)이거나 일반외과(GS)인 의사의 이름, 의사ID, 진료과, 고용일자를 조회하는 SQL문을 작성해주세요. 
+# 이때 결과는 고용일자를 기준으로 내림차순 정렬하고, 고용일자가 같다면 이름을 기준으로 오름차순 정렬해주세요.
+
+SELECT DR_NAME, DR_ID, MCDP_CD, TO_CHAR(HIRE_YMD, 'YYYY-MM-DD') AS HIRE_YMD
+FROM DOCTOR 
+WHERE MCDP_CD = 'CS' OR MCDP_CD = 'GS' # WHERE MCDP_CD IN ('CS', 'GS')
+ORDER BY HIRE_YMD DESC, DR_NAME
