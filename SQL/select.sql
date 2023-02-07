76f7d22a3951991c65af7bcc82b40f3b7fe7bec7
eunsooklee <98075420+loulou-lee@users.noreply.github.com>
2023-02-07 18:52:00 +0900

    Update select.sql

--- SQL/select.sql
+++ SQL/select.sql
@@ -1,12 +1,10 @@
 # 평균 일일 대여 요금 구하기
-
 SELECT ROUND(AVG(DAILY_FEE)) AS AVERAGE_FEE
 FROM CAR_RENTAL_COMPANY_CAR
 WHERE CAR_TYPE = 'SUV'
 
 # DOCTOR 테이블에서 진료과가 흉부외과(CS)이거나 일반외과(GS)인 의사의 이름, 의사ID, 진료과, 고용일자를 조회하는 SQL문을 작성해주세요. 
 # 이때 결과는 고용일자를 기준으로 내림차순 정렬하고, 고용일자가 같다면 이름을 기준으로 오름차순 정렬해주세요.
-
 SELECT DR_NAME, DR_ID, MCDP_CD, TO_CHAR(HIRE_YMD, 'YYYY-MM-DD') AS HIRE_YMD
 FROM DOCTOR 
 WHERE MCDP_CD = 'CS' OR MCDP_CD = 'GS' # WHERE MCDP_CD IN ('CS', 'GS')
@@ -18,3 +16,8 @@ SELECT USER_ID, PRODUCT_ID
 FROM ONLINE_SALE 
 GROUP BY USER_ID, PRODUCT_ID HAVING COUNT(PRODUCT_ID) >= 2 
 ORDER BY USER_ID, PRODUCT_ID DESC
+
+# 동물 보호소에 들어온 모든 동물의 이름과 보호 시작일을 조회하는 SQL문을 작성해주세요. 이때 결과는 ANIMAL_ID 역순으로 보여주세요.
+SELECT NAME, DATETIME 
+FROM ANIMAL_INS 
+ORDER BY ANIMAL_ID DESC
