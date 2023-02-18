e4ca466318227ad4893a832a59ef97b6cae1cd1f
eunsooklee <98075420+loulou-lee@users.noreply.github.com>
2023-02-18 21:29:45 +0900

    Update join.sql

--- SQL/join.sql
+++ SQL/join.sql
@@ -18,5 +18,8 @@ ORDER BY SALES desc, PRODUCT_CODE
 # MEMBER_PROFILE와 REST_REVIEW 테이블에서 리뷰를 가장 많이 작성한 회원의 리뷰들을 조회하는 SQL문을 작성해주세요. 
 # 회원 이름, 리뷰 텍스트, 리뷰 작성일이 출력되도록 작성해주시고, 
 # 결과는 리뷰 작성일을 기준으로 오름차순, 리뷰 작성일이 같다면 리뷰 텍스트를 기준으로 오름차순 정렬해주세요.
+SELECT MEMBER_NAME, REVIEW_TEXT, REVIEW_DATE
+FROM MEMBER_PROFILE a, REST_REVIEW b
+WHERE
 
 # 집계함수(COUNT) 안에 또다른 집계함수(MAX)를 중첩해서 사용할 수 없다.
