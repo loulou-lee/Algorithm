7be4a4bcd52e8b9771e64e9e9dc0f9aa021c5479
eunsooklee <98075420+loulou-lee@users.noreply.github.com>
2023-02-18 20:44:47 +0900

    Update select.sql

--- SQL/select.sql
+++ SQL/select.sql
@@ -1,4 +1,6 @@
 # 평균 일일 대여 요금 구하기
+# CAR_RENTAL_COMPANY_CAR 테이블에서 자동차 종류가 'SUV'인 자동차들의 평균 일일 대여 요금을 출력하는 SQL문을 작성해주세요. 
+# 이때 평균 일일 대여 요금은 소수 첫 번째 자리에서 반올림하고, 컬럼명은 AVERAGE_FEE 로 지정해주세요.
 SELECT ROUND(AVG(DAILY_FEE)) AS AVERAGE_FEE
 FROM CAR_RENTAL_COMPANY_CAR
 WHERE CAR_TYPE = 'SUV'
@@ -21,3 +23,10 @@ ORDER BY USER_ID, PRODUCT_ID DESC
 SELECT NAME, DATETIME 
 FROM ANIMAL_INS 
 ORDER BY ANIMAL_ID DESC
+
+#동물 보호소에 들어온 동물 중 아픈 동물1의 아이디와 이름을 조회하는 SQL 문을 작성해주세요. 
+# 이때 결과는 아이디 순으로 조회해주세요.
+SELECT ANIMAL_ID, NAME 
+FROM ANIMAL_INS
+WHERE INTAKE_CONDITION = 'Sick'
+ORDER BY ANIMAL_ID
